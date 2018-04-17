cd50d2ee851fdd53ad7b480693e0d95d2efabe8c
李琪琦 <445072942@qq.com>
2018-04-17 16:05:19 +0800

    修改归一化 增加放大裁剪 缩小裁剪

--- normalization.py
+++ normalization.py
@@ -15,43 +15,66 @@ def face_detection(detector,file):
     print("Detected faces:",dets)
     for k,d in enumerate(dets):
         #print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(k, d.left(), d.top(), d.right(), d.bottom()))
-        leftTop = (int(d.left()),int(d.top()))
+        leftTop = (int(d.left()), int(d.top()))
         rightBottom = (int(d.right()), int(d.bottom()))
         #cvRectangle函数参数： 图片， 左上角， 右下角， 颜色， 线条粗细， 线条类型，点类型
         #cv2.rectangle(img, leftTop, rightBottom, (0, 0, 255), 1, 4, 0)
         #cv2.imshow('face',img)
         #cv2.waitKey(0)
 
-        #裁剪出感兴趣区域：
-        detected_region = img[d.top():d.bottom(), d.left():d.right()]
-        detected_region = cv2.resize(detected_region, (256, 256))
+
+        #放大脸部区域
+        a1=x1=int(d.left())
+        a2=x2=int(d.right())
+        b1=y1=int(d.top())
+        b2=y2=int(d.bottom())
+
+        rows = img.shape[0]
+        cols = img.shape[1]
+
+        a1= (x1-(x2-x1)/4) if (x1-(x2-x1)/4)>=0 else 0
+        b1= (y1-(y2-y1)/4) if (y1-(y2-y1)/4)>=0 else 0
+        a2= (x2+(x2-x1)/4) if (x2+(x2-x1)/4)<cols else cols
+        b2= (y2+(y2-y1)/4) if (y2+(y2-y1)/4)<rows else rows
+
+        #优化--裁剪出感兴趣区域（放大脸部区域后裁剪）：
+        detected_region=img[b1:b2,a1:a2,:]
+        detected_region=cv2.resize(detected_region, (256, 256))
+
+        #cv2.imshow('detected_region', detected_region)
+        #cv2.waitKey(0)
+
+        #裁剪出感兴趣区域（直接裁剪）：
+        #detected_region = img[d.top():d.bottom(), d.left():d.right()]
+        #detected_region = cv2.resize(detected_region, (256, 256))
 
         str = file.split('/')
         saveImageName = str[len(str)-1]
         #print(saveImageName)
         cv2.imwrite(os.getcwd() + "/preprocess/detected/"+saveImageName, detected_region)
-    return saveImageName, img, dets
+    return saveImageName, detected_region
 
 #第2步：关键点对齐(仿射变换)
-def face_landmarks(predictor, saveImageName, img, dets, detector):
-    for k, d in enumerate(dets):
+def face_landmarks(predictor, saveImageName, img, detector):
+    new_dets = detector(img, 1)
+    for k, d in enumerate(new_dets):
         shape = predictor(img, d)
         image = dlib.get_face_chip(img, shape)
-        cv_bgr_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv_bgr_img = cv2.resize(image, (256, 256))
 
         #输出旋转图片
-        #cv2.imshow('landmarks_pic',cv_bgr_img)
+        #cv2.imshow('aligned_pic',cv_bgr_img)
         #cv2.waitKey(0)
+        cv2.imwrite(os.getcwd() +"/preprocess/aligned/"+saveImageName, cv_bgr_img)
 
-        #裁剪中间区域，避免旋转造成的锯齿边缘：先检测旋转归一后的感兴趣区域，根据矩形框裁剪,new_dets是旋转后的检测人脸框
-        new_dets = detector(cv_bgr_img, 1)
 
-        #确定只有一张脸：
-        new_d = new_dets[0]
-        croped_img = cv_bgr_img[new_d.top():new_d.bottom(), new_d.left():new_d.right()]
+        #缩小裁剪区域
+        rows = cv_bgr_img.shape[0]
+        cols = cv_bgr_img.shape[1]
+        croped_img = cv_bgr_img[rows/8:rows*7/8,cols/8:cols*7/8,:]
         croped_img = cv2.resize(croped_img, (256, 256))
-        cv2.imshow('cropped_pic', croped_img)
-        cv2.waitKey(0)
+        #cv2.imshow('cropped_pic', croped_img)
+        #cv2.waitKey(0)
         print(cv2.imwrite(os.getcwd() +"/preprocess/cropped/"+saveImageName, croped_img))
 
 
@@ -79,8 +102,9 @@ def face_landmarks(predictor, saveImageName, img, dets, detector):
 def normalize(faces_folder_path,detector,predictor):
     files = glob.glob(os.path.join(faces_folder_path, "*.jpg"))
     for file in files:
-        saveImageName, img, dets = face_detection(detector,file)
-        face_landmarks(predictor, saveImageName, img, dets, detector)
+    #file = "/Users/Vivien/Documents/论文/论文实验/归一化/归一化结果数据集/Dataset/origin/KA.SA3.35.jpg"
+        saveImageName, img = face_detection(detector,file)
+        face_landmarks(predictor, saveImageName, img, detector)
 
 
 
@@ -88,3 +112,5 @@ if __name__ =="__main__":
     faces_folder_path = "/Users/Vivien/Documents/论文/论文实验/归一化/归一化结果数据集/Dataset/origin"
     detector,predictor = face_detector.get_face_detector()
     normalize(faces_folder_path, detector, predictor)
+
+
